bd5e1054a901427b085b3357fbe41615e0040e77
Хамелеон <maxali946@gmail.com>
2017-12-14 19:47:30 +0400

    15%

--- Moneyfy/Moneyfy/Functions.cs
+++ Moneyfy/Moneyfy/Functions.cs
@@ -17,10 +17,20 @@ namespace Monefy
 
             return instance;
         }
+        //--------------------------------------------------------
+        char[] DoubleLine = { (char)201, (char)205, (char)187, (char)186, (char)188, (char)205, (char)200, (char)186};
 
-        void MainFrame()
+        char[] SingleLine = { (char)218, (char)196, (char)191, (char)179, (char)217, (char)196, (char)192, (char)179 };
+        //--------------------------------------------------------
+        void MainFrame(char type)
         {
-            
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine(DoubleLine[0]);
+            for (int i = 0; i < length; i++)
+            {
+
+            }
         }
     }
 }
